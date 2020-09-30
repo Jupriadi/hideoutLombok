55caa488ded017323379284c32a6bc4515873506
Jupriadi <jpashter@gmail.com>
2020-10-01 01:15:11 +0700

    modifikasi file package

--- src/components/pacakageList.jsx
+++ src/components/pacakageList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PackageList =(props)=>{
     return(
-        <div className="card" style={{width: "15rem"}}>
+        <div className="card shadow-sm" style={{width: "15rem"}}>
             <img src={require('../img/svg/beach.jpg')} className="card-img-top" alt="Gambar Paket" />
             <div className="card-body">
                 <h5 className="card-title">{props.judul}</h5>
